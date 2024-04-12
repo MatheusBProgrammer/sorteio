04338dde0b819920cf2f05f1b7ce4f420cd58099
MatheusBProgrammer <matheusbarretoprogramador@gmail.com>
2024-04-12 19:33:52 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,46 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [firstNumber, setFirstNumber] = useState("");
-  const [secondNumber, setSecondNumber] = useState("");
+  const [minNumber, setMin] = useState("");
+  const [maxNumber, setMax] = useState("");
+  const [listLength, setListLength] = useState("");
+
   const [result, setResult] = useState(null);
   const handleFirstNumberChange = (event) => {
-    const value = event.target.value.replace(/\D/, ""); // Remove caracteres que não são dígitos
-    setFirstNumber(value);
+    const value = event.target.value.replace(/\D/, "");
+    setMin(value);
   };
 
   const handleSecondNumberChange = (event) => {
-    const value = event.target.value.replace(/\D/, ""); // Remove caracteres que não são dígitos
-    setSecondNumber(value);
+    const value = event.target.value.replace(/\D/, "");
+    setMax(value);
+  };
+
+  const handleListLengthChange = (event) => {
+    let value = event.target.value.replace(/\D/, "");
+    setListLength(value);
   };
 
   const handleDraw = () => {
-    const min = Math.ceil(parseInt(firstNumber));
-    const max = Math.floor(parseInt(secondNumber));
-    const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-    setResult(randomNumber);
+    const min = Math.ceil(parseInt(minNumber));
+    const max = Math.floor(parseInt(maxNumber));
+    const length = parseInt(listLength);
+
+    if (max < min) {
+      alert("O segundo número deve ser maior ou igual ao primeiro número.");
+      return;
+    }
+    if (!minNumber || !maxNumber || !listLength) {
+      alert("Você precisa escolher números válidos em todos os campos");
+    }
+
+    const resultList = [];
+    for (let i = 0; i < length; i++) {
+      const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+      resultList.push(randomNumber);
+    }
+
+    setResult(resultList.join(", "));
   };
 
   return (
@@ -32,7 +54,7 @@ function App() {
             <input
               type="number"
               id="firstValueField"
-              value={firstNumber}
+              value={minNumber}
               onChange={handleFirstNumberChange}
               min="0"
             />
@@ -42,9 +64,22 @@ function App() {
             <input
               type="number"
               id="secondValueField"
-              value={secondNumber}
+              value={maxNumber}
               onChange={handleSecondNumberChange}
               min="0"
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="listLength">
+              Quantidade de Números a sortear{` (no Máximo 20)`}:
+            </label>
+            <input
+              type="number"
+              id="listLength"
+              value={listLength}
+              onChange={handleListLengthChange}
+              min="1"
+              max="20"
             />
           </div>
           <button type="button" onClick={handleDraw}>
@@ -54,7 +89,7 @@ function App() {
         <div className="result">
           {result !== null && (
             <p>
-              O número sorteado entre {firstNumber} e {secondNumber} é:
+              Os números sorteados entre {minNumber} e {maxNumber} são:
               <br />
               <span className="resultSpan">{result}</span>
             </p>
